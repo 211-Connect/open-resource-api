66f69c2ad1c87c68efc67b8e35097af9090d8942
Chris <13677134+devcshort@users.noreply.github.com>
2021-09-08 11:51:03 -0700

    Remove session route reference

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -7,7 +7,6 @@ import taxonomy from './taxonomy';
 import search from './search';
 import service from './service';
 import organization from './organization';
-import session from './session';
 
 const router = new Router({
   prefix: '/api/v1',
@@ -20,6 +19,5 @@ router.use(taxonomy.routes()).use(taxonomy.allowedMethods());
 router.use(search.routes()).use(search.allowedMethods());
 router.use(service.routes()).use(service.allowedMethods());
 router.use(organization.routes()).use(organization.allowedMethods());
-router.use(session.routes()).use(session.allowedMethods());
 
 export default router;
